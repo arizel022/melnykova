a700c60fa016a64b8badf7a90b4fd6f685f70c1e
Alex Mykhailov <arizel022@gmail.com>
2026-08-10 23:10:19 -0400

    fixed mix-blend-mode

--- docs/js/main.js
+++ docs/js/main.js
@@ -28,6 +28,30 @@
 
 
 
+(() => {
+    const header = document.querySelector('.header');
+    if (!header) return;
+
+    let ticking = false;
+
+    function updatePosition() {
+        header.style.top = `${window.scrollY}px`;
+        ticking = false;
+    }
+
+    window.addEventListener('scroll', () => {
+        if (!ticking) {
+            requestAnimationFrame(updatePosition);
+            ticking = true;
+        }
+    }, { passive: true });
+
+    updatePosition(); // стартовая позиция при загрузке
+})();
+
+
+
+
 
 (() => {
     const projects = document.querySelector('.projects');
